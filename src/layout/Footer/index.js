efc4fdd13aa31064b99562d0146ee0d600ca5224
ephalys <ephalys@live.fr>
2020-02-21 13:05:15 +0100

    add: footer

--- src/layout/Footer/index.js
+++ src/layout/Footer/index.js
@@ -1,11 +1,32 @@
 import React, {Component} from 'react';
 import './footer.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebookF, faTwitter, faLinkedinIn } from "@fortawesome/free-brands-svg-icons"
+import logoFooter from '../../assets/images/logo-footer.svg'
 
 class Header extends Component {
     render() {
         return (
-            <footer>
-
+            <footer className="footer">
+                <div className="footer__content">
+                    <div className="footer__content-logo">
+                        <img src="logoFooter" alt="logo-footer"/>
+                    </div>
+                    <div className="footer__content-copyright">
+                        <p>copyright 2020</p>
+                    </div>
+                    <div className="footer__content-socials">
+                        <a href="www.facebook.com" className="footer__content-socials-link">
+                            <FontAwesomeIcon icon={faFacebookF} />
+                        </a>
+                        <a href="www.facebook.com" className="footer__content-socials-link">
+                            <FontAwesomeIcon icon={faTwitter} />
+                        </a>
+                        <a href="www.facebook.com" className="footer__content-socials-link">
+                            <FontAwesomeIcon icon={faLinkedinIn} />
+                        </a>
+                    </div>
+                </div>
             </footer>
         );
     }
