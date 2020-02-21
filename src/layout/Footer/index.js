f05bec1f71b489bba4b9b1f39e6c09d7d9712c25
ephalys <ephalys@live.fr>
2020-02-21 12:06:34 +0100

    add: header and footer

---
+++ src/layout/Footer/index.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import './footer.scss'
+
+class Header extends Component {
+    render() {
+        return (
+            <footer>
+
+            </footer>
+        );
+    }
+}
+
+export default Header;
