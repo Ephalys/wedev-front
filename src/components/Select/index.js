914a0136529943a5d8b44e4a418a24a9c8423936
ephalys <ephalys@live.fr>
2020-02-22 15:14:04 +0100

    add: Theme for react select

--- src/components/Select/index.js
+++ src/components/Select/index.js
@@ -1,5 +1,22 @@
 import React, {Component} from 'react';
 import ReactSelect from 'react-select'
+const customStyles = {
+    indicatorsContainer: () => ({
+        // none of react-select's styles are passed to <Control />
+        backgroundColor: '#f1f1f1',
+        borderColor: 'transparent'
+    }),
+    control: (provided) => ({
+        ...provided,
+        // none of react-select's styles are passed to <Control />
+        backgroundColor: '#f1f1f1',
+        borderColor: 'transparent',
+        cursor: 'pointer',
+        '&:hover': {
+            borderColor: 'transparent',
+        }
+    }),
+};
 
 class Select extends Component {
     render() {
@@ -7,6 +24,14 @@ class Select extends Component {
             <div className="form-group">
                 <label htmlFor="">{this.props.label}</label>
                 <ReactSelect
+                    styles={customStyles}
+                    theme={(theme) => ({
+                        ...theme,
+                        colors: {
+                            ...theme.colors,
+                            primary: '#2ECC71',
+                        },
+                    })}
                     options={this.props.values}
                 />
             </div>
