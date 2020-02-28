5a7dc37a262299c6b773a3661a2429997601de76
ephalys <ephalys@live.fr>
2020-02-28 13:20:51 +0100

    wip: champs register

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -2,18 +2,22 @@ import React, { Component } from "react";
 import "./input.scss";
 
 class Input extends Component {
-  render() {
-    return (
-      <div className="form-group">
-        <label>{this.props.label}</label>
-        <input
-          name={this.props.nameField}
-          type={this.props.type}
-          placeholder={this.props.placeholder}
-        />
-      </div>
-    );
-  }
+
+    render() {
+        return (
+            <div className="form-group">
+                <label>{this.props.label}</label>
+                <input
+                    name={this.props.nameField}
+                    type={this.props.type}
+                    placeholder={this.props.placeholder}
+                    onChange={(event => {
+                        this.props.changed(event)
+                    })}
+                />
+            </div>
+        );
+    }
 }
 
 export default Input;
--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Input from "../../components/Input";
 import "./register.scss";
 import Select from "../../components/Select";
+import axios from 'axios';
 
 const profileList = [
   { value: "back", label: "Back-end" },
@@ -19,6 +20,33 @@ const companyStatus = [
 ];
 
 class Register extends Component {
+
+  onInputChange = (event, field) => {
+    this.setState({
+      field: event.target.value
+    });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    const user = {
+      firstName: event.target.value,
+      lastName: this.state.lastName,
+      mail: this.state.mail,
+      plainPassword: this.state.lastName,
+      lastName: this.state.lastName,
+      lastName: this.state.lastName,
+      lastName: this.state.lastName,
+    };
+
+    axios.post(`http://10.92.4.45:3000/register`, { firstName: user.firstName })
+        .then(res => {
+          console.log(res);
+          console.log(res.data);
+        })
+  };
+
   render() {
     return (
       <div className="register">
@@ -30,47 +58,58 @@ class Register extends Component {
               label="Firstname"
               type="text"
               placeholder="John"
+              changed={this.onInputChange(event, 'firstName')}
             />
             <Input
               nameField="lastName"
               label="Lastname"
               type="email"
               placeholder="Doe"
+              changed={this.onInputChange(event, 'lastName')}
             />
             <Input
               nameField="plainPassword"
               label="Password"
               type="password"
               placeholder="Must have at least 6 characters"
+              changed={this.onInputChange(event, 'plainPassword')}
             />
             <Input
               nameField="phone"
               label="Phone"
               type="phone"
               placeholder="01 23 45 67 89"
+              changed={this.onInputChange(event, 'phone')}
             />
-            <Select nameField="profile" values={profileList} label="Profile" />
+            <Select nameField="profile"
+                    values={profileList} label="Profile"
+                    changed={this.onInputChange(event, 'profile')}
+            />
             <Input
               nameField="companyName"
               label="Company name"
               type="phone"
               placeholder="WeDev"
+              changed={this.onInputChange(event, 'companyName')}
             />
             <Input
               nameField="companySiret"
               label="SIRET"
               type="phone"
               placeholder="example@gmail.com"
+              changed={this.onInputChange(event, 'companySiret')}
             />
             <Select
               nameField="companyStatus"
               values={companyStatus}
               label="Company Status"
+              changed={this.onInputChange(event, 'companyStatus')}
             />
             <input
               type="submit"
               value="Register"
               className="btn btn__rounded btn__green btn__letter-spacing fwb"
+              onClick={this.handleSubmit}
             />
           </form>
         </div>
