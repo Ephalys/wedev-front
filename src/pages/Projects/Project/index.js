8a8878b931dab8e495ed233ae8acf8849e48e55b
subskill-ltrnvn <l.tran-van@subskill.com>
2020-03-20 15:07:17 +0100

    fix: UI projects

--- src/pages/Projects/Project/index.js
+++ src/pages/Projects/Project/index.js
@@ -2,26 +2,25 @@ import React, { Component } from "react";
 import "./project.scss";
 
 class Project extends Component {
-  render() {
-    return (
-      <div className="project" onClick={this.props.click}>
-        <p>{this.props.title}</p>
-        <p>{this.props.amount}</p>
-        <p>{this.props.delay}</p>
-        <p>{this.props.startDate}</p>
-        <p>{this.props.EndDate}</p>
-        <p>{this.props.status}</p>
-        <p>{this.props.stacks}</p>
-        <p>{this.props.adr}</p>
-        <p>{this.props.client}</p>
-        <p>{this.props.user}</p>
-        {this.BlogPost}
-        <div>
-          <a href={`/dashboard/detailsproject/${this.props.id}`}>detail</a>
-        </div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="project" onClick={this.props.click}>
+                <div className="project__header">
+                    <strong><a href={`/dashboard/detailsproject/${this.props.id}`}>{this.props.title}</a></strong>
+                    <p>{this.props.status}</p>
+                </div>
+                <p>{this.props.amount} €</p>
+                <p>{this.props.delay}</p>
+                <p>{this.props.startDate}</p>
+                <p>{this.props.EndDate}</p>
+                <p>{this.props.stacks}</p>
+                <p>{this.props.adr}</p>
+                <p>{this.props.client}</p>
+                <p>{this.props.user}</p>
+                {this.BlogPost}
+            </div>
+        );
+    }
 }
 
 export default Project;
