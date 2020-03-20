ed9d3ec91e61f9abec9375f6fc0cb0b6e5ba3b23
Bastien P <bastien.pinard@ynov.com>
2020-03-20 15:19:48 +0100

    add : sprint update

--- src/pages/Projects/Project/index.js
+++ src/pages/Projects/Project/index.js
@@ -2,25 +2,28 @@ import React, { Component } from "react";
 import "./project.scss";
 
 class Project extends Component {
-    render() {
-        return (
-            <div className="project" onClick={this.props.click}>
-                <div className="project__header">
-                    <strong><a href={`/dashboard/detailsproject/${this.props.id}`}>{this.props.title}</a></strong>
-                    <p>{this.props.status}</p>
-                </div>
-                <p>{this.props.amount} €</p>
-                <p>{this.props.delay}</p>
-                <p>{this.props.startDate}</p>
-                <p>{this.props.EndDate}</p>
-                <p>{this.props.stacks}</p>
-                <p>{this.props.adr}</p>
-                <p>{this.props.client}</p>
-                <p>{this.props.user}</p>
-                {this.BlogPost}
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="project" onClick={this.props.click}>
+        <div className="project__header">
+          <strong>
+            <a href={`/dashboard/detailsproject/${this.props.id}`}>
+              {this.props.title}
+            </a>
+          </strong>
+          <p>{this.props.status}</p>
+        </div>
+        <p>{this.props.amount} €</p>
+        <p>{this.props.delay}</p>
+        <p>{this.props.startDate}</p>
+        <p>{this.props.EndDate}</p>
+        <p>{this.props.stacks}</p>
+        <p>{this.props.adr}</p>
+        <p>{this.props.client}</p>
+        <p>{this.props.user}</p>
+      </div>
+    );
+  }
 }
 
 export default Project;
