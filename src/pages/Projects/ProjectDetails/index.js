d1b2ac53dd834eefcd720a87508e90d489a1ceb7
subskill-ltrnvn <l.tran-van@subskill.com>
2020-03-25 23:13:53 +0100

    fix: UI details project

--- src/pages/Projects/ProjectDetails/index.js
+++ src/pages/Projects/ProjectDetails/index.js
@@ -15,14 +15,14 @@ class DetailsProject extends Component {
 
   componentDidMount() {
     axios
-      .get("/project/" + this.props.match.params.id, {
-        headers: {
-          Authorization: localStorage.getItem("token")
-        }
-      })
-      .then(response => {
-        this.setState({ project: response.data.project });
-      });
+        .get("/project/" + this.props.match.params.id, {
+          headers: {
+            Authorization: localStorage.getItem("token")
+          }
+        })
+        .then(response => {
+          this.setState({ project: response.data.project });
+        });
   }
   render() {
     console.log(this.state);
@@ -32,105 +32,71 @@ class DetailsProject extends Component {
     if (this.state.project.Sprints) {
       sprints = this.state.project.Sprints.map((element, i) => {
         return (
-          <Sprint
-            key={i}
-            id={element.id}
-            title={element.title}
-            startDate={element.startDate}
-            endDate={element.endDate}
-            status={element.status}
-          />
+            <Sprint
+                key={i}
+                id={element.id}
+                title={element.title}
+                startDate={element.startDate}
+                endDate={element.endDate}
+                status={element.status}
+            />
         );
       });
     }
 
     return (
-      <div>
-        <h1>Aperçu du projet — {this.state.project.title}</h1>
-        <form>
-          <Input
-            nameField="title"
-            label="Title"
-            type="text"
-            placeholder=""
-            valueField={this.state.project ? this.state.project.title : ""}
-            changed={this.onInputChange}
-            isDisabled={this.state.isDisabled}
-          />
-          <Input
-            nameField="amount"
-            label="Amount"
-            type="text"
-            placeholder=""
-            valueField={this.state.project ? this.state.project.amount : ""}
-            changed={this.onInputChange}
-            isDisabled={this.state.isDisabled}
-          />
-          <Input
-            nameField="delay"
-            label="Delay"
-            type="text"
-            placeholder=""
-            valueField={this.state.project ? this.state.project.delay : ""}
-            changed={this.onInputChange}
-            isDisabled={this.state.isDisabled}
-          />
-          <Input
-            nameField="startDate"
-            label="StartDate"
-            type="text"
-            placeholder=""
-            valueField={this.state.project ? this.state.project.startDate : ""}
-            changed={this.onInputChange}
-            isDisabled={this.state.isDisabled}
-          />
-          <Input
-            nameField="endDate"
-            label="EndDate"
-            type="text"
-            placeholder=""
-            valueField={this.state.project ? this.state.project.endDate : ""}
-            changed={this.onInputChange}
-            isDisabled={this.state.isDisabled}
-          />
-          <Input
-            nameField="status"
-            label="Status"
-            type="text"
-            placeholder=""
-            valueField={this.state.project ? this.state.project.status : ""}
-            changed={this.onInputChange}
-            isDisabled={this.state.isDisabled}
-          />
-          <Input
-            nameField="stacks"
-            label="Stacks"
-            type="text"
-            placeholder=""
-            valueField={this.state.project ? this.state.project.stacks : ""}
-            changed={this.onInputChange}
-            isDisabled={this.state.isDisabled}
-          />
-          <Input
-            nameField="adr"
-            label="hourly rate"
-            type="text"
-            placeholder=""
-            valueField={this.state.project ? this.state.project.adr : ""}
-            changed={this.onInputChange}
-            isDisabled={this.state.isDisabled}
-          />
-        </form>
+        <div className="project__details">
+          <h1>Aperçu du projet — {this.state.project.title}</h1>
+          <div className="project__details-cards">
+            <div className="project__details-block">
+              {this.state.project ? this.state.project.status : ""}
+            </div>
+            <div className="project__details-block">
+              {this.state.project ? this.state.project.stacks : ""}
+            </div>
+            <div className="project__details-block">
+              {this.state.project ? this.state.project.amount : ""} €
+            </div>
+          </div>
+          <form>
+            <Input
+                nameField="startDate"
+                label="StartDate"
+                type="text"
+                placeholder=""
+                valueField={this.state.project ? this.state.project.startDate : ""}
+                changed={this.onInputChange}
+                isDisabled={this.state.isDisabled}
+            />
+            <Input
+                nameField="endDate"
+                label="EndDate"
+                type="text"
+                placeholder=""
+                valueField={this.state.project ? this.state.project.endDate : ""}
+                changed={this.onInputChange}
+                isDisabled={this.state.isDisabled}
+            />
+            <Input
+                nameField="adr"
+                label="hourly rate"
+                type="text"
+                placeholder=""
+                valueField={this.state.project ? this.state.project.adr : ""}
+                changed={this.onInputChange}
+                isDisabled={this.state.isDisabled}
+            />
+          </form>
 
-        <div className="sprintsList">{sprints}</div>
-        <div>
-          <a
-            href={`/dashboard/detailsproject/createsprint/${this.props.match.params.id}`}
-          >
-            create sprint
-          </a>
+          <div className="sprintsList">{sprints}</div>
+          <div>
+            <a
+                href={`/dashboard/detailsproject/createsprint/${this.props.match.params.id}`}
+            >
+              create sprint
+            </a>
+          </div>
         </div>
-      </div>
     );
   }
 }
